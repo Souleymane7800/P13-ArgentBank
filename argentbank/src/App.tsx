6b26678fe6731b225dae8ac968bfe5130422bfc3
Souleymane7800 <souleymane.doumbia7800@gmail.com>
2024-07-03 10:14:34 +0200

    hero

--- argentbank/src/App.tsx
+++ argentbank/src/App.tsx
@@ -1,17 +1,16 @@
-
-import './App.css'
-import Navbar from './components/Navbar'
+import './App.css';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
 
 function App() {
-
-
-  return (
-    <div>
-      <Navbar />
-
-    </div>
-    
-  )
+      return (
+            <>
+                  <Navbar />
+                  <main>
+                        <Hero />
+                  </main>
+            </>
+      );
 }
 
-export default App
+export default App;
